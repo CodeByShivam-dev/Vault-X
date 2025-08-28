ba323ca643e6978da98676eaf3979557afadd5d1
CodeByShivam-dev <shivamkumar.dev2006@gmail.com>
2025-08-28 23:55:26 +0530

    Updated VaultX project with Twilio token placeholders

--- src/com/vaultx/otp/TwilioConfig.java
+++ src/com/vaultx/otp/TwilioConfig.java
@@ -3,9 +3,9 @@ package com.vaultx.otp;
 import com.twilio.Twilio;
 
 public class TwilioConfig {
-    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" +
+    public static final String ACCOUNT_SID = "zyz" +
             "\n";
-    public static final String AUTH_TOKEN = "Auth_token";
+    public static final String AUTH_TOKEN = "Auth_token_here";
 
     static {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
