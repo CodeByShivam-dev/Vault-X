45b3fb8c453e2d59537175e1c5cd14118bcf6a28
CodeByShivam-dev <shivamkumar.dev2006@gmail.com>
2025-08-28 07:51:47 +0530

    Added Twilio API integration and VaultX console app update

--- src/com/vaultx/otp/OtpMainApp.java
+++ src/com/vaultx/otp/OtpMainApp.java
@@ -1,4 +1,18 @@
 package com.vaultx.otp;
 
+import java.util.Scanner;
+
 public class OtpMainApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your WhatsApp phone number (with country code, e.g. 919876543210): ");
+        String userPhoneNumber = scanner.nextLine();
+
+        OtpService otpService = new OtpService();
+        otpService.sendOtp(userPhoneNumber);
+
+        scanner.close();
+    }
 }
--- src/com/vaultx/otp/OtpService.java
+++ src/com/vaultx/otp/OtpService.java
@@ -1,4 +1,30 @@
 package com.vaultx.otp;
+import com.vaultx.otp.TwilioConfig;
 
-public class OtpService {
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import java.util.Random;
+public class OtpService
+{
+
+    public static final String ACCOUNT_SID = TwilioConfig.ACCOUNT_SID;
+    public static final String AUTH_TOKEN = TwilioConfig.AUTH_TOKEN;
+
+    public void sendOtp(String toWhatsAppNumber)
+    {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        int otp = 100000 + new Random().nextInt(900000);
+        String messageBody = "Your VaultX verification code is: " + otp;
+
+        Message message = Message.creator(
+                        new PhoneNumber("whatsapp:" + toWhatsAppNumber),
+                        new PhoneNumber("whatsapp:+14155238886"),
+                        messageBody)
+                .create();
+
+        System.out.println("OTP sent with SID: " + message.getSid());
+    }
 }
